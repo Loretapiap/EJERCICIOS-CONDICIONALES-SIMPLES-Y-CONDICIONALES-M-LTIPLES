3f04363410f6e6aa1e4c248848c868e5e849600c
Loretapiap <loretapiap@gmail.com>
2016-06-17 12:23:47 -0400

    Todos los ejercicios resueltos

--- Ejercicio 10 FABRICAS/Problema/main.js
+++ Ejercicio 10 FABRICAS/Problema/main.js
@@ -1,1 +1,41 @@
-//Escribe aquí tu código
+var n1 = prompt("ingrese clave de articulo: 1, 2, 3, 4, 5 o 6");
+while (!/^([1-6])*$/.test(n1)){
+n1 = prompt("el valor ingresado no está dentro del rango, ingrese nuevamente");
+}
+var n2 = prompt("ingrese costo de materia prima");
+while (!/^([0-9])*$/.test(n2)){
+n2 = prompt("el valor ingresado no es un numero, ingrese nuevamente");
+}
+
+if (n1=="1") {
+	var costo = parseInt(n2) + (parseInt(n2) + parseInt(n2)*0.8) + (parseInt(n2) + parseInt(n2)*0.28);
+	document.write("Su presupuesto es: $" + costo)
+	;}
+
+if (n1=="2") {
+	var costo = parseInt(n2) + (parseInt(n2) + parseInt(n2)*0.85) + (parseInt(n2) + parseInt(n2)*0.3);
+	document.write("Su presupuesto es: $" + costo)
+	;}
+
+if (n1=="3") {
+	var costo = parseInt(n2) + (parseInt(n2) + parseInt(n2)*0.75) + (parseInt(n2) + parseInt(n2)*0.35);
+	document.write("Su presupuesto es: $" + costo)
+	;}
+
+if (n1=="4") {
+	var costo = parseInt(n2) + (parseInt(n2) + parseInt(n2)*0.75) + (parseInt(n2) + parseInt(n2)*0.28);
+	document.write("Su presupuesto es: $" + costo)
+	;}
+
+if (n1=="5") {
+	var costo = parseInt(n2) + (parseInt(n2) + parseInt(n2)*0.8) + (parseInt(n2) + parseInt(n2)*0.3);
+	document.write("Su presupuesto es: $" + costo)
+	;}
+
+if (n1=="6") {
+	var costo = parseInt(n2) + (parseInt(n2) + parseInt(n2)*0.85) + (parseInt(n2) + parseInt(n2)*0.35);
+	document.write("Su presupuesto es: $" + costo)
+	;}
+
+
+
